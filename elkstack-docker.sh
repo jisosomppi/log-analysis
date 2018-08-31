57116e2c18efadadc8ab8f40e6190c63ff9184fe
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-08-31 11:42:35 +0300

    Create elkstack-docker.sh

---
+++ elkstack-docker.sh
@@ -0,0 +1,16 @@
+## One click installer for ELK Stack containers (https://github.com/deviantony/docker-elk)
+## Jussi Isosomppi, 2018
+
+## Install docker-compose, needed to build the containers
+sudo apt-get install -y docker-compose
+
+## Clone git repository
+cd
+git clone https://github.com/deviantony/docker-elk
+
+## Start the stack (-d to detach the containers and free up the terminal)
+cd docker-elk
+sudo docker-compose up -
+
+## Test functionality
+curl -I localhost:5601
