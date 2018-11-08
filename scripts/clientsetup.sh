a3108f72f585b421fb900830bc7690b0ebfc1bc1
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-11-08 13:29:32 +0000

    Update clientsetup.sh

--- scripts/clientsetup.sh
+++ scripts/clientsetup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Simple client setup
+sudo apt-get update && sudo apt-get install salt-minion
 echo "Please enter master IP address here:"
 read MasterIP
 echo "Please enter a unique id for your system:"
