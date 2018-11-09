336d25692cf06da05288ab0a58f3923dc4d847d2
Jussi Isosomppi <jussi.isosomppi@gmail.com>
2018-11-09 16:56:34 +0200

    rework scripts

---
+++ salt/serversetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Simple logging server
+
+echo "Updating packages..."
+sudo apt-get update -qq >> /dev/null
+echo "Installing git and salt-minion..."
+sudo apt-get install git salt-minion -y -qq >> /dev/null
+echo "Cloning repository..."
+git clone https://github.com/jisosomppi/log-analysis
+echo "Running automated setup.."
+echo "(This will take a while)"
+sudo cp log-analysis/salt/saltmaster /etc/salt/master
+sudo systemctl restart salt-minion
+sudo salt-call --local --file-root log-analysis/salt/srvsalt --pillar-root log-analysis/salt/srvpillar state.highstate --state-output terse
+echo "Setup is now complete! Access the Kibana logging frontend at http://localhost"
