4f5ed0d8f03408366da8437933c6fbf1e38cd594
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-11-09 17:20:49 +0200

    Update serversetup.sh

--- salt/serversetup.sh
+++ salt/serversetup.sh
@@ -3,13 +3,26 @@
 
 echo "Updating packages..."
 sudo apt-get update -qq >> /dev/null
-echo "Installing git and salt-minion..."
-sudo apt-get install git salt-minion -y -qq >> /dev/null
+echo "Installing git and salt..."
+sudo apt-get install git salt-master salt-minion -y -qq >> /dev/null
 echo "Cloning repository..."
 git clone https://github.com/jisosomppi/log-analysis
 echo "Running automated setup.."
 echo "(This will take a while)"
-sudo cp log-analysis/salt/saltmaster /etc/salt/master
+sudo cp log-analysis/salt/saltmaster /etc/salt/minion
 sudo systemctl restart salt-minion
 sudo salt-call --local --file-root log-analysis/salt/srvsalt --pillar-root log-analysis/salt/srvpillar --id srv01 state.highstate --state-output terse -l quiet
-echo "Setup is now complete! Access the Kibana logging frontend at http://localhost"
+echo -e "\nfile_ignore_glob: []\n" | sudo tee -a /etc/salt/master
+sudo service salt-master restart 
+
+
+
+
+
+echo "Server setup is now complete!"
+echo ""
+echo "Access the Kibana logging frontend at http://localhost"
+echo "Client logs will be found in /var/log/client_logs"
+echo ""
+echo "Run 'sudo salt srv01 state.apply fixperms' when new"
+echo "host directories or log files are created"
