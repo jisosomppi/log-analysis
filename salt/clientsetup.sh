4f387841ec3e307315a7858155af2a1a5975f3be
Saku Kähäri <31246291+nauskis@users.noreply.github.com>
2018-11-14 14:38:40 +0200

    Update clientsetup.sh

--- salt/clientsetup.sh
+++ salt/clientsetup.sh
@@ -13,3 +13,5 @@ echo "Writing information to /etc/salt/minion..."
 echo -e "master: $MasterIP\nid: ws-$SystemID" | sudo tee /etc/salt/minion
 echo "Restarting salt-minion..."
 sudo systemctl restart salt-minion
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
