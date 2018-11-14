41f96072dff31b28a23e0a1af63f9bc0787f2682
Saku Kähäri <31246291+nauskis@users.noreply.github.com>
2018-11-14 12:46:19 +0000

    Update serversetup.sh

--- salt/serversetup.sh
+++ salt/serversetup.sh
@@ -31,3 +31,5 @@ echo "Run 'sudo salt srv01 state.apply fixperms' when new host directories or lo
 echo
 echo "Opening the logging frontend at http://logserver.local (This address also works on connected Salt minions)"
 firefox http://logserver.local
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
