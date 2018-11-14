01d2281a62dc040c901e8c6eba11ddb029184cd1
Saku Kähäri <31246291+nauskis@users.noreply.github.com>
2018-11-14 15:17:09 +0200

    Update serversetup.sh

--- salt/serversetup.sh
+++ salt/serversetup.sh
@@ -9,7 +9,7 @@ sudo apt-get update -qq >> /dev/null
 echo "Installing git and salt..."
 sudo apt-get install git salt-master salt-minion -y -qq >> /dev/null
 echo "Cloning repository..."
-git clone https://github.com/jisosomppi/log-analysis/tree/sakutesti
+git clone -b sakutesti https://github.com/jisosomppi/log-analysis/
 echo "Running automated setup... (This will take a while)"
 if [ ! -d "/srv/" ]; then
 sudo mkdir /srv/
