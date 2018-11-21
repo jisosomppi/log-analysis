d59f286d06d278f2ce7d0ba7add3c88d0243f8a6
einokupias <31246369+einokupias@users.noreply.github.com>
2018-11-21 15:02:43 +0200

    Fixed Jussis broken script

--- scripts/vagrantup.sh
+++ scripts/vagrantup.sh
@@ -1,6 +1,6 @@
 # fast vagrant script
 
-sudo apt-get install vagrant virtualbox
+sudo apt-get install -y vagrant virtualbox
 mkdir ~/vagrant && cd ~/vagrant
 vagrant init bento/ubuntu-16.04
 vagrant up
