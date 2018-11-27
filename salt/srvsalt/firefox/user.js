e4cb2a9e7eaf5a6f38851c856b522f4d19cb25bd
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-11-27 11:46:29 +0000

    Create user.js

---
+++ salt/srvsalt/firefox/user.js
@@ -0,0 +1,4 @@
+## THIS IS A CENTRALLY MANAGED FILE! ##
+##    CHANGES WILL BE OVERWRITTEN    ##
+
+user_pref("browser.startup.homepage", "https://github.com/jisosomppi/log-analysis|https://logserver.local");
